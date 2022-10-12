9ddb47e44a1aefe3b62156da95071dac7b07670b
Dominik Pilipczuk <dominikpil123@gmail.com>
2022-10-12 11:24:57 +0200

    Added MapDirection & MoveDirection enums.

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -3,8 +3,8 @@ package agh.ics.oop;
 public class World {
 
 
-    public static void run(Direction.Directions[] kierunki){
-        for (Direction.Directions kierunek : kierunki) {
+    public static void run(MoveDirection[] kierunki){
+        for (MoveDirection kierunek : kierunki) {
             String message = switch (kierunek){
                 case FORWARD -> "Zwierzak idzie do przodu\n";
                 case BACKWARD -> "Zwierzak idzie do tyłu\n";
@@ -19,24 +19,34 @@ public class World {
 
     public static void main(String[] args){
         int n = args.length;
-        Direction.Directions[] kierunki = new Direction.Directions[n];
+        MoveDirection[] kierunki = new MoveDirection[n];
         for (int i = 0; i < n; i++){
             kierunki[i] = switch (args[i]){
-                case "f" -> Direction.Directions.FORWARD;
-                case "b" -> Direction.Directions.BACKWARD;
-                case "r" -> Direction.Directions.RIGHT;
-                case "l" -> Direction.Directions.LEFT;
+                case "f" -> MoveDirection.FORWARD;
+                case "b" -> MoveDirection.BACKWARD;
+                case "r" -> MoveDirection.RIGHT;
+                case "l" -> MoveDirection.LEFT;
                 default -> null;
             };
         }
+
         System.out.println("system wystartował");
-//        World.run(kierunki);
+
+        World.run(kierunki);
 
         Vector2d position1 = new Vector2d(1,2);
         System.out.println(position1);
         Vector2d position2 = new Vector2d(-2,1);
         System.out.println(position2);
         System.out.println(position1.add(position2));
+
+//        MapDirection mapD = MapDirection.NORTH;
+//
+//        for (int i = 0; i < 4; i++){
+//            System.out.println(mapD);
+//            System.out.println(mapD.toUnitVector());
+//            mapD = mapD.previous();
+//        }
 
         System.out.println("system zakończył działanie");
     }

22d848f16321b549dafe29d0e191ae4069dda77f
Dominik Pilipczuk <dominikpil123@gmail.com>
2022-10-17 15:58:45 +0200

    Test

---
+++ src/main/java/agh/ics/oop/Animal.java
@@ -0,0 +1,56 @@
+package agh.ics.oop;
+
+public class Animal {
+    private MapDirection orientation = MapDirection.NORTH;
+    private Vector2d position = new Vector2d(2,2);
+
+    private final Vector2d lowerLeft = new Vector2d(0,0);
+    private final Vector2d upperRight = new Vector2d(4, 4);
+
+    public String toString(){
+        return "Zwierzak jest na pozycji " + position.toString() + ", w kierunku " + orientation.toString() + ".";
+    }
+
+    public boolean isAt(Vector2d position){
+        return this.position.equals(position);
+    }
+
+    public void move(MoveDirection direction){
+        if (direction == MoveDirection.FORWARD){
+            Vector2d tmp = this.position.add(switch (this.orientation){
+                case NORTH -> new Vector2d(0,1);
+                case EAST -> new Vector2d(1, 0);
+                case SOUTH -> new Vector2d(0,-1);
+                case WEST -> new Vector2d(-1,0);
+            });
+
+            if (upperRight.lowerLeft(tmp).equals(tmp) && lowerLeft.upperRight(tmp).equals(tmp)){
+                this.position = tmp;
+            }
+        }
+
+        if (direction == MoveDirection.RIGHT){
+            this.orientation = this.orientation.next();
+            return;
+        }
+
+        if (direction == MoveDirection.LEFT){
+            this.orientation = this.orientation.previous();
+            return;
+        }
+
+        if (direction == MoveDirection.BACKWARD){
+            Vector2d tmp = this.position.add(switch (this.orientation){
+                case NORTH -> new Vector2d(0,-1);
+                case EAST -> new Vector2d(-1, 0);
+                case SOUTH -> new Vector2d(0,1);
+                case WEST -> new Vector2d(1,0);
+            });
+
+            if (upperRight.lowerLeft(tmp).equals(tmp) && lowerLeft.upperRight(tmp).equals(tmp)){
+                this.position = tmp;
+            }
+        }
+    }
+
+}
--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -1,53 +1,14 @@
 package agh.ics.oop;
 
 public class World {
-
-
-    public static void run(MoveDirection[] kierunki){
-        for (MoveDirection kierunek : kierunki) {
-            String message = switch (kierunek){
-                case FORWARD -> "Zwierzak idzie do przodu\n";
-                case BACKWARD -> "Zwierzak idzie do tyłu\n";
-                case RIGHT -> "Zwierzak skręca w prawo\n";
-                case LEFT -> "Zwierzak skręca w lewo\n";
-                default -> "";
-            };
-            System.out.print(message);
-        }
-
-    }
-
     public static void main(String[] args){
-        int n = args.length;
-        MoveDirection[] kierunki = new MoveDirection[n];
-        for (int i = 0; i < n; i++){
-            kierunki[i] = switch (args[i]){
-                case "f" -> MoveDirection.FORWARD;
-                case "b" -> MoveDirection.BACKWARD;
-                case "r" -> MoveDirection.RIGHT;
-                case "l" -> MoveDirection.LEFT;
-                default -> null;
-            };
-        }
+        Animal zwierzak = new Animal();
 
-        System.out.println("system wystartował");
+        zwierzak.move(MoveDirection.RIGHT);
+        zwierzak.move(MoveDirection.FORWARD);
+        zwierzak.move(MoveDirection.FORWARD);
+        zwierzak.move(MoveDirection.FORWARD);
 
-        World.run(kierunki);
-
-        Vector2d position1 = new Vector2d(1,2);
-        System.out.println(position1);
-        Vector2d position2 = new Vector2d(-2,1);
-        System.out.println(position2);
-        System.out.println(position1.add(position2));
-
-//        MapDirection mapD = MapDirection.NORTH;
-//
-//        for (int i = 0; i < 4; i++){
-//            System.out.println(mapD);
-//            System.out.println(mapD.toUnitVector());
-//            mapD = mapD.previous();
-//        }
-
-        System.out.println("system zakończył działanie");
+        System.out.println(zwierzak);
     }
 }

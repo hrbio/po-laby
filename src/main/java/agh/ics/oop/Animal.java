eca873295789ddc1bf8b6873375a42b72ec01f3d
Dominik Pilipczuk <dominikpil123@gmail.com>
2022-10-24 01:02:23 +0200

    Koniec labow

--- src/main/java/agh/ics/oop/Animal.java
+++ src/main/java/agh/ics/oop/Animal.java
@@ -1,25 +1,38 @@
 package agh.ics.oop;
 
 public class Animal {
+
+
     private MapDirection orientation = MapDirection.NORTH;
     private Vector2d position = new Vector2d(2,2);
-
+    private IWorldMap map;
     private final Vector2d lowerLeft = new Vector2d(0,0);
     private final Vector2d upperRight = new Vector2d(4, 4);
+//    public Animal(IWorldMap map){
+//        this.map = map;
+//    }
+//
+//    public Animal(IWorldMap map, Vector2d position){
+//        this.map = map;
+//        this.position = position;
+//    }
 
     public String toString(){
-        return "Zwierzak jest na pozycji " + position.toString() + ", w kierunku " + orientation.toString() + ".";
+        return switch (orientation){
+            case NORTH -> "^";
+            case EAST -> ">";
+            case SOUTH -> "v";
+            case WEST -> "<";
+        };
     }
 
     public boolean isAt(Vector2d position){
         return this.position.equals(position);
     }
-
     public void move(MoveDirection direction){
         if (direction == MoveDirection.FORWARD){
             Vector2d tmp = this.position.add(this.orientation.toUnitVector());
-
-            if (upperRight.lowerLeft(tmp).equals(tmp) && lowerLeft.upperRight(tmp).equals(tmp)){
+            if (map.canMoveTo(tmp)){
                 this.position = tmp;
             }
         }
@@ -36,8 +49,7 @@ public class Animal {
 
         if (direction == MoveDirection.BACKWARD){
             Vector2d tmp = this.position.subtract(this.orientation.toUnitVector());
-
-            if (upperRight.lowerLeft(tmp).equals(tmp) && lowerLeft.upperRight(tmp).equals(tmp)){
+            if (map.canMoveTo(tmp)){
                 this.position = tmp;
             }
         }
--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -4,19 +4,7 @@ public class World {
 
     private static OptionsParser optionsParser = new OptionsParser();
     public static void main(String[] args){
-
-        String[] kierunki = {"r", "r", "r", "r", "r", "r"};
-
-        MoveDirection[] directions = optionsParser.parse(kierunki);
-
         Animal antek = new Animal();
-
-        for (MoveDirection direction : directions){
-            antek.move(direction);
-        }
-
         System.out.println(antek);
-        System.out.println("Antek się kręci :)");
-
     }
 }

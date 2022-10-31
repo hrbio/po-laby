d4284e6ea15dd02b188effbb9e0ba74982475435
Dominik Pilipczuk <dominikpil123@gmail.com>
2022-10-31 14:40:19 +0100

    Lab 3

--- src/test/java/agh/ics/oop/IntegrationTest.java
+++ src/test/java/agh/ics/oop/IntegrationTest.java
@@ -1,13 +1,13 @@
-//package agh.ics.oop;
-//
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//
-//public class IntegrationTest {
-//    private final OptionsParser optionsParser = new OptionsParser();
-//
-//    @Test
-//    void testOne(){
+package agh.ics.oop;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class IntegrationTest {
+    private final OptionsParser optionsParser = new OptionsParser();
+
+    @Test
+    void testOne(){
 //        Animal tomek = new Animal();
 //        Assertions.assertTrue(tomek.isAt(new Vector2d(2,2)));
 //        String[] kierunki = {"r", "r", "b", "b"};
@@ -18,10 +18,10 @@
 //            tomek.move(direction);
 //        }
 //        Assertions.assertTrue(tomek.isAt(new Vector2d(2,4)));
-//    }
-//
-//    @Test
-//    void testTwo(){
+    }
+
+    @Test
+    void testTwo(){
 //        Animal blazej = new Animal();
 //        Assertions.assertTrue(blazej.isAt(new Vector2d(2,2)));
 //        String[] kierunki = {"left", "f", "f", "right", "backward"};
@@ -32,10 +32,10 @@
 //            blazej.move(direction);
 //        }
 //        Assertions.assertTrue(blazej.isAt(new Vector2d(0,1)));
-//    }
-//
-//    @Test
-//    void testThree(){
+    }
+
+    @Test
+    void testThree(){
 //        Animal pawel = new Animal();
 //        Assertions.assertTrue(pawel.isAt(new Vector2d(2,2)));
 //        String[] kierunki = {"b", "r", "right", "backward", "left", "right", "forward", "b", "l"};
@@ -46,10 +46,10 @@
 //            pawel.move(direction);
 //        }
 //        Assertions.assertTrue(pawel.isAt(new Vector2d(2,2)));
-//    }
-//
-//    @Test
-//    void testFour(){
+    }
+
+    @Test
+    void testFour(){
 //        Animal grzesiu = new Animal();
 //        Assertions.assertTrue(grzesiu.isAt(new Vector2d(2,2)));
 //        String[] kierunki = {"left", "backward", "l", "backward", "f", "f", "f"};
@@ -60,10 +60,10 @@
 //            grzesiu.move(direction);
 //        }
 //        Assertions.assertTrue(grzesiu.isAt(new Vector2d(3,0)));
-//    }
-//
-//    @Test
-//    void testFive(){
+    }
+
+    @Test
+    void testFive(){
 //        Animal gerwazy = new Animal();
 //        Assertions.assertTrue(gerwazy.isAt(new Vector2d(2,2)));
 //        String[] kierunki = {"b", "backward", "b", "backward", "b", "l", "f", "f", "f"};
@@ -74,5 +74,5 @@
 //            gerwazy.move(direction);
 //        }
 //        Assertions.assertTrue(gerwazy.isAt(new Vector2d(0,0)));
-//    }
-//}
+    }
+}

2d32c968885c69ef445647ba5e97971b5b3cbc3b
Dominik Pilipczuk <dominikpil123@gmail.com>
2022-10-31 14:40:19 +0100

    Lab 4

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -7,9 +7,9 @@ public class World {
 
     private static OptionsParser optionsParser = new OptionsParser();
     public static void main(String[] args){
-        MoveDirection[] directions = optionsParser.parse(args);
+        MoveDirection[] directions = new OptionsParser().parse(args);
         IWorldMap map = new RectangularMap(10, 5);
-        Vector2d[] positions = { new Vector2d(2,2), new Vector2d(3,4)};
+        Vector2d[] positions = { new Vector2d(2,2), new Vector2d(3,4) };
         IEngine engine = new SimulationEngine(directions, map, positions);
         engine.run();
         System.out.println(map.toString());
